8a7224482f2b9ed73a2bbfd6ea1f76d19ce3ce57
David Eldridge <david.eldridge1987@gmail.com>
2021-07-26 15:25:43 +0930

    Delete tokens.py

--- tokens.py
+++
@@ -1,5 +0,0 @@
-bot_token = "ODY5MDAwNDAyNDcxODk1MDgw.YP31sQ.jUeMgnDBXLiRw0HhKB4ybFaBLS8"
-client_id = "869000402471895080"
-
-
-bot_link = "https://discord.com/api/oauth2/authorize?client_id=869000402471895080&permissions=211008&scope=bot"
